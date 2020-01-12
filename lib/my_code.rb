c314efc39ad71aa12773361473d3c25750644354
Jeremy Garnes <j06garnes@gmail.com>
2020-01-12 21:30:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,10 +52,10 @@ def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
     if source_array[i] == source_array[i]
-      true 
+      false  
     end 
     i += 1 
   end 
-  false 
+  true
 end   
 
