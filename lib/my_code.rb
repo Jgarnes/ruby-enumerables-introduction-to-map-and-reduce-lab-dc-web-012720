f1ec845ff829aec45deb29e0b2570c470f496b66
Jeremy Garnes <j06garnes@gmail.com>
2020-01-12 22:00:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -61,9 +61,8 @@ def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.length do 
     return true 
-    if !source_array[i]
-      return false 
-    end 
+    if source_array[i]
       i += 1 
     end 
+    return false
   end   
