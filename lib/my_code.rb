49c18d0870fe5acfc467163a07895642b30134b7
Jeremy Garnes <j06garnes@gmail.com>
2020-01-12 19:15:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize(source_array)
   new_array = []
   i = 0 
   while i < source_array do 
-    total = ([i] * -1) += source_array
+    total = source_array[i]
   new_array << total 
   end 
  new_array
