5fcb21ad0a7f38ce671dcaa52d1a9c6994059920
Jeremy Garnes <j06garnes@gmail.com>
2020-01-12 19:17:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@
 def map_to_negativize(source_array)
   new_array = []
   i = 0 
-  while i < source_array do 
+  while i < source_array.length do 
     total = source_array[i]
   new_array << total 
   end 
