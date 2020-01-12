04a335a00613ecc9a20609b7b372c23cde6dc294
Jeremy Garnes <j06garnes@gmail.com>
2020-01-12 19:32:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,3 +35,13 @@ def map_to_square(source_array)
    end 
   new_array
 end 
+
+
+def reduce_to_total(source_array,starting_point)
+ i = 0
+  while i < source_array.length
+   total = 0 += source_array[i]
+   i += 1 
+  end 
+ total  
+end 
